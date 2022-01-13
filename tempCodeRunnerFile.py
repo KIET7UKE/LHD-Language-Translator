97264839e86b2e257d6135615bc26002cef3e87d
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-01-13 20:30:23 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+#second combobox
+# combo2=ttk.Combobox(root,values=languageV,font="Roboto 14", state="r")
+# combo2.place(x=110,y=20)
+# combo2.set("ENGLISH")
+
+# label2=Label(root,text="ENGLISH",font="segoe 30 bold",bg="white",width=18,bd=5,relief=GROOVE)
+# label2.place(x=62,y=50)
